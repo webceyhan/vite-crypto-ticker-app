25961a9e1976b72d2e1ce02d94da2902e49321e1
Evren Ceyhan <evren@ceyhan.io>
2022-02-11 00:16:24 +0300

    ticker refactored

--- src/components/Ticker.vue
+++ src/components/Ticker.vue
@@ -11,10 +11,10 @@ defineProps({
 </script>
 
 <template>
-    <div class="card text-light">
+    <div class="card text-light bg-secondary bg-opacity-25 border-secondary">
         <div class="row g-0">
             <div class="col">
-                <div class="h5 bg-secondary text-capitalize m-0 p-3">{{ symbol }}</div>
+                <div class="h6 text-capitalize m-0 p-3">{{ symbol }}</div>
             </div>
             <div class="col text-dark">
                 <div
