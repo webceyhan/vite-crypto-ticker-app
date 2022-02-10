86adf940e25739941d30666be8e01dcfee7570a0
Evren Ceyhan <evren@ceyhan.io>
2022-02-10 18:27:24 +0300

    bootstrap added with basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,12 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <header class="container text-center my-3">
+    <h1 class="display-4">Vite Crypto Ticker App</h1>
+  </header>
+
+  <section class="container">body here</section>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
